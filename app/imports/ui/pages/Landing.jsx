180156f76c2b7dd05a9ff8d633d9501f12c4287a
Zachary Chaikin <zchaikin@hawaii.edu>
2022-04-11 21:04:41 -1000

    Done

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Grid, Header, Icon, Image } from 'semantic-ui-react';
+import { Link, Redirect } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const buttonStyle = { backgroundColor: 'red', color: 'white', fontFamily: 'Arial', fontSize: '30px', marginLeft: '100px', marginRight: '100px' };
+    const headStyle = { color: 'red', fontFamily: 'UHMFont', fontSize: '40px', fontWeight: '700', paddingBottom: '30px' };
+    const logoStyle = { color: 'red', fontFamily: 'UHMFont', fontSize: '300px', fontWeight: '700', paddingTop: '100px' };
+    const sumStyle = { color: 'red', fontFamily: 'Arial', fontSize: '30px', fontWeight: '700', marginLeft: '170px', marginRight: '170px', paddingBottom: '100px', paddingTop: '30px' };
     return (
       <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
+        <Grid.Row>
+          <Header as='h1' style={logoStyle}>WC</Header>
+        </Grid.Row>
+        <Grid.Row>
+          <Header as='h1' style={headStyle}>Warrior Cravings </Header>
+        </Grid.Row>
+        <Grid.Row>
+          <Button className="ui circular red button" color='white'as={NavLink} exact to="/signin" style={buttonStyle}>Sign In</Button>
+          <Button className="ui circular red button" color='white' as={NavLink} exact to="/signup" style={buttonStyle}>Sign Up</Button>
+        </Grid.Row>
+        <Grid.Row>
+          <p style={sumStyle}>The University of Hawaiʻi at Mānoa campus offers a large selection of food places for students to enjoy and eat at. Warrior Cravings allows Students to see all the unique food choices available on campus while providing a platform for Vendors to showcase their menu. </p>
+        </Grid.Row>
       </Grid>
+
     );
   }
 }
