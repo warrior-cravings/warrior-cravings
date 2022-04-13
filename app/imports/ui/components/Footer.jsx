fd45731b511ba89a3880ebc7e2c0c463070e91cf
Louie808 <97556345+Louie808@users.noreply.github.com>
2022-04-12 23:39:22 -1000

    Nav/Footer waiting on accounts to test.  Added possible logo

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Grid, Menu, Icon, List } from 'semantic-ui-react';
 
-/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const footStyle = { backgroundColor: 'red', height: '100px', paddingTop: '10x', paddingBottom: '10px' };
-    const footContentStyle = { color: 'white', fontSize: '30px' };
+    const divStyle = { paddingTop: '20px', paddingBottom: '15px' };
     return (
-      <footer className="footer" attached="bottom" style={footStyle}>
-        <div style={footContentStyle} className="ui center aligned container">
+      <footer>
+        <div style={divStyle} className="ui center aligned container">
+          <hr/>
               The Warrior Cravings Project <br/>
               University of Hawaii<br/>
               Honolulu, HI 96822 <br/>
-          <a style={footContentStyle} href="https://github.com/warrior-cravings">https://github.com/warrior-cravings</a>
+          <a href="https://github.com/warrior-cravings">https://github.com/warrior-cravings</a>
         </div>
       </footer>
     );
