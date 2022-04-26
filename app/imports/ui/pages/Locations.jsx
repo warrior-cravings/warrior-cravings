0a11a99aea5b6f2316c41b993d2879f3d2d50432
Cknakano <Cknakano@hawaii.edu>
2022-04-26 12:08:13 -1000

    issue 21 locations

--- app/imports/ui/pages/Locations.jsx
+++ app/imports/ui/pages/Locations.jsx
@@ -15,17 +15,17 @@ class Locations extends React.Component {
               <h2 className="ui red header">Auxillary Services</h2>
               <div className="ui divider"></div>
               <List.Content>
-                <List.Header>Business Administration</List.Header>
+                <List.Header>1. Business Administration</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/the-bean-counter//">The Bean Counter</a></List.Item>
               </List.Content>
               <Divider hidden />
               <List.Content>
-                <List.Header>Saunders</List.Header>
+                <List.Header>2. Saunders</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/da-spot/">Da Spot</a></List.Item>
               </List.Content>
               <Divider hidden />
               <List.Content>
-                <List.Header>Paradise Palms Café</List.Header>
+                <List.Header>3. Paradise Palms Café</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/dunkin/">Dunkin Donuts</a></List.Item>
                 <List.Item><a href="https://manoa.hawaii.edu/food/holoholo-grill/">Holoholo Grill</a></List.Item>
                 <List.Item><a href="https://manoa.hawaii.edu/food/ll-hawaiian-barbecue/">L&L Barbecue</a></List.Item>
@@ -35,18 +35,18 @@ class Locations extends React.Component {
               </List.Content>
               <Divider hidden />
               <List.Content>
-                <List.Header>Krauss</List.Header>
+                <List.Header>9. Krauss</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/ono-seafood/">Ono Seafood</a></List.Item>
                 <List.Item><a href="https://manoa.hawaii.edu/food/kamitoku-ramen/">Kamitoku Ramen</a></List.Item>
               </List.Content>
               <Divider hidden />
               <List.Content>
-                <List.Header>POST</List.Header>
+                <List.Header>11. POST</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/kamitoku-ramen/">Kamitoku Ramen</a></List.Item>
               </List.Content>
               <Divider hidden />
               <List.Content>
-                <List.Header>Athletic Complex</List.Header>
+                <List.Header>14. Athletic Complex</List.Header>
                 <List.Item><a href="https://manoa.hawaii.edu/food/ll-hawaiian-barbecue/">L&L Barbecue</a></List.Item>
               </List.Content>
             </List>
@@ -58,31 +58,31 @@ class Locations extends React.Component {
                 <h2 className="ui yellow header">Rotating Truck</h2>
                 <div className="ui divider"></div>
                 <List.Content>
-                  <List.Header>Korean Studies</List.Header>
+                  <List.Header>4. Korean Studies</List.Header>
                   <List.Item><a href="https://manoa.hawaii.edu/food/sistah/">Sistah Truck (Thursday)</a></List.Item>
                   <List.Item><a href="https://manoa.hawaii.edu/food/hot-tacos/">Hot Tacos (Friday)</a></List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Architecture</List.Header>
+                  <List.Header>5. Architecture</List.Header>
                   <List.Item><a href="https://manoa.hawaii.edu/food/sistah/">Sistah Truck (Wednesday)</a></List.Item>
                   <List.Item><a href="https://manoa.hawaii.edu/food/hot-tacos/">Hot Tacos (Thursday)</a></List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Kuykendall</List.Header>
+                  <List.Header>8. Kuykendall</List.Header>
                   <List.Item><a href="https://manoa.hawaii.edu/food/sistah/">Sistah Truck (Tuesday)</a></List.Item>
                   <List.Item><a href="https://manoa.hawaii.edu/food/hot-tacos/">Hot Tacos (Wednesday)</a></List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Kennedy Theatre</List.Header>
+                  <List.Header>10. Kennedy Theatre</List.Header>
                   <List.Item><a href="https://manoa.hawaii.edu/food/sistah/">Sistah Truck (Friday)</a></List.Item>
                   <List.Item><a href="https://manoa.hawaii.edu/food/hot-tacos/">Hot Tacos (Monday)</a></List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>POST 2</List.Header>
+                  <List.Header>12. POST 2</List.Header>
                   <List.Item><a href="https://manoa.hawaii.edu/food/sistah/">Sistah Truck (Monday)</a></List.Item>
                   <List.Item><a href="https://manoa.hawaii.edu/food/hot-tacos/">Hot Tacos (Tuesday)</a></List.Item>
                 </List.Content>
@@ -92,12 +92,12 @@ class Locations extends React.Component {
                 <h2 className="ui blue header">Student Affairs</h2>
                 <div className="ui divider"></div>
                 <List.Content>
-                  <List.Header>Hemenway</List.Header>
+                  <List.Header>6. Hemenway</List.Header>
                   <List.Item>Ba-Le</List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Campus Center</List.Header>
+                  <List.Header>7. Campus Center</List.Header>
                   <List.Item>Food Court</List.Item>
                   <List.Item>Jamba Juice</List.Item>
                   <List.Item>Pizza Hut</List.Item>
@@ -106,17 +106,17 @@ class Locations extends React.Component {
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Gateway House</List.Header>
+                  <List.Header>13. Gateway House</List.Header>
                   <List.Item>Gateway Cafe</List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Hale Aloha 2</List.Header>
+                  <List.Header>15. Hale Aloha 2</List.Header>
                   <List.Item>Hale Aloha Cafe</List.Item>
                 </List.Content>
                 <Divider hidden />
                 <List.Content>
-                  <List.Header>Hale Noelani</List.Header>
+                  <List.Header>16. Hale Noelani</List.Header>
                   <List.Item>The Market</List.Item>
                 </List.Content>
                 <Divider hidden />
