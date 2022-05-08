bb7f71e89278456bfdd81553e53c2087b152e106
mahi3crab <mahi3@hawaii.edu>
2022-05-08 08:14:16 -1000

    fix home pages

--- app/imports/ui/components/HomePage/VendorHome.jsx
+++ app/imports/ui/components/HomePage/VendorHome.jsx
@@ -8,7 +8,7 @@ class VendorHome extends React.Component {
     const middlePadding = { paddingTop: '100px', paddingBottom: '100px' };
     return (
       <div style={middlePadding}>
-        <Grid id='landing-page' container centered stackable columns={2}>
+        <Grid id='landing-page' container centered stackable columns={3}>
           <Grid.Column textAlign='center'>
             <Link to="/listVenU">
               <Icon color='red' size="massive" name="utensils"/>
