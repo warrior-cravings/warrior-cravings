712f34487819ae5c2e6b566da4905ba4450e0b72
Zachary Chaikin <zchaikin@hawaii.edu>
2022-05-09 22:36:10 -1000

    TestCafe

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -2,6 +2,12 @@ import { landingPage } from './landing.page';
 import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
 import { navBar } from './navbar.component';
+import { feelingHungryPage } from './feelinghungry.page';
+import { homePage } from './home.page';
+import { listMenuItemsAdminPage } from './listmenuitemsadmin.page';
+import { listMenuItemsUserPage } from './listmenuitemsuser.page';
+import { listMenuItemsVendorPage } from './listmenuitemsvendor.page';
+import { locationPage } from './location.page';
 
 /* global fixture:false, test:false */
 
@@ -13,6 +19,30 @@ fixture('meteor-application-template-react localhost test with default db')
 
 test('Test that landing page shows up', async (testController) => {
   await landingPage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await feelingHungryPage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await homePage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await listMenuItemsAdminPage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await listMenuItemsUserPage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await listMenuItemsVendorPage.isDisplayed(testController);
+});
+
+test('Test that landing page shows up', async (testController) => {
+  await locationPage.isDisplayed(testController);
 });
 
 test('Test that signin and signout work', async (testController) => {
