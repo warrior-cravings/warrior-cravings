2a1e29404b3811d193007198cf50be3a1dc1d6a6
Zachary Chaikin <zchaikin@hawaii.edu>
2022-05-10 10:28:54 -1000

    TestCafe

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -16,14 +16,13 @@ import { myVendorPage } from './myvendor.page';
 import { vendorHomePage } from './vendorhome.page';
 import { menuItemsVendorPage } from './menuitemsvendor.page';
 
-
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
 const credentialsA = { username: 'admin@foo.com', password: 'changeme' };
 const credentialsV = { username: 'bean@foo.com', password: 'changeme' };
-const createMenuItem = { name: 'croissant', mealType: 'Dessert', ingredients: '[flour, butter, water, yeast, sugar, salt, egg, milk]' };
+// const createMenuItem = { name: 'croissant', mealType: 'Dessert', ingredients: '[flour, butter, water, yeast, sugar, salt, egg, milk]' };
 
 fixture('meteor-application-template-react localhost test with default db')
   .page('http://localhost:3000');
